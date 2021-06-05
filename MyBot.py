036dcaee6b0aa8d06faeb019e9d80bf972c8fa0f
Patrick Timm <patze2000@freenet.de>
2021-06-05 12:14:18 +0200

    Modifying Starter Bot. Raumschiffe fliegen jetzt glaube in unterschiedliche Richtungen.

--- MyBot.py
+++ MyBot.py
@@ -17,9 +17,14 @@ import logging
 
 # GAME START
 # Here we define the bot's name as Settler and initialize the game, including communication with the Halite engine.
-game = hlt.Game("Settler")
+game = hlt.Game("Settler")  #umbennen in Shabab ? :D
 # Then we print our start message to the logs
 logging.info("Starting my Settler bot!")
+
+# Versuch, dass nicht alle Raumschiffe in die selbe Richtung fliegen
+
+planned_planets = [] # hinzugefügt
+
 
 while True:
     # TURN START
@@ -45,29 +50,47 @@ while True:
             # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
             if ship.can_dock(planet):
                 # We add the command by appending it to the command_queue
+
+
                 command_queue.append(ship.dock(planet))
-            else:
-                # If we can't dock, we move towards the closest empty point near this planet (by using closest_point_to)
-                # with constant speed. Don't worry about pathfinding for now, as the command will do it for you.
-                # We run this navigate command each turn until we arrive to get the latest move.
-                # Here we move at half our maximum speed to better control the ships
-                # In order to execute faster we also choose to ignore ship collision calculations during navigation.
-                # This will mean that you have a higher probability of crashing into ships, but it also means you will
-                # make move decisions much quicker. As your skill progresses and your moves turn more optimal you may
-                # wish to turn that option off.
-                navigate_command = ship.navigate(
-                    ship.closest_point_to(planet),
-                    game_map,
-                    speed=int(hlt.constants.MAX_SPEED/2),
-                    ignore_ships=True)
+            else: # hinzugefügt
+
+                if planet in planned_planets:
+                    continue
+
+                else:
+                    # If we can't dock, we move towards the closest empty point near this planet (by using closest_point_to)
+                    # with constant speed. Don't worry about pathfinding for now, as the command will do it for you.
+                    # We run this navigate command each turn until we arrive to get the latest move.
+                    # Here we move at half our maximum speed to better control the ships
+                    # In order to execute faster we also choose to ignore ship collision calculations during navigation.
+                    # This will mean that you have a higher probability of crashing into ships, but it also means you will
+                    # make move decisions much quicker. As your skill progresses and your moves turn more optimal you may
+                    # wish to turn that option off.
+                    navigate_command = ship.navigate(
+                        ship.closest_point_to(planet),
+                        game_map,
+                        speed=int(hlt.constants.MAX_SPEED/2),
+                        ignore_ships=True)
                 # If the move is possible, add it to the command_queue (if there are too many obstacles on the way
                 # or we are trapped (or we reached our destination!), navigate_command will return null;
                 # don't fret though, we can run the command again the next turn)
-                if navigate_command:
+                if navigate_command: # Prüfe, ob die if Schleife die Richtige Einrückung hat
                     command_queue.append(navigate_command)
+                    planned_planets.append(planet) #hinzugefügt
             break
 
     # Send our set of commands to the Halite engine for this turn
     game.send_command_queue(command_queue)
     # TURN END
 # GAME END
+"""The idea here is that first we get all of our ships, then we're looking for any ships not currently docked. 
+Next, we iterate through all of the planets, looking for a planet that doesn't have docked ships at it (making them "owned"). 
+If our ship can dock, we attempt to dock, entering this action into a "command queue." 
+The idea here with the command queue is that you might have many actions you want to take per "turn." 
+You are controlling many ships. If we cannot dock, then we want to start looking for a new planet again, and begin navigating to it as our command. 
+In order to "dock" at a planet, you need to be within close proximity. Now, each game turn takes place within the while True loop. 
+So you send all of your commands ONCE per iteration of the main game loop (the top-level while True). 
+Once the game ends, the while True will be broken and the game is over. 
+https://pythonprogramming.net/introduction-halite-ii-artificial-intelligence-competition/
+"""
